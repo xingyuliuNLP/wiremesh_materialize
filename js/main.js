7bd7bcef54559a1bcc4287ee9a30f660a7f6949e
xingyuliuNLP <xingyuliufrance@gmail.com>
2020-02-15 15:48:30 +0100

    2019

---
+++ js/main.js
@@ -0,0 +1,119 @@
+// initialization
+document.addEventListener('DOMContentLoaded', function () {
+  // scrollSpy
+  var scrollSpy = document.querySelectorAll('.scrollspy');
+  var instance1 = M.ScrollSpy.init(scrollSpy, {});
+  // sideNav
+  // var elems = document.querySelectorAll('.sidenav');
+  // var instances = M.Sidenav.init(elems, options);
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var sideNav = document.querySelectorAll('.sidenav');
+  var instance2 = M.Sidenav.init(sideNav, {});
+});
+
+
+
+<title>
+      Hengshui Qualified Filter Screen Co., Ltd. - printing mesh, polyester
+      screen meshBeijing, polyester printing mesh, polyester filter belt
+    </title>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </head>
+  <!-- navbar -->
+  <body id="home" class="scrollspy">
+    <div class="navbar-fixed">
+      <nav class="brown lighten-1">
+        <div class="container">
+          <div class="nav-wrapper">
+            <a href="#home" class="brand-logo">HS Filter Screen</a>
+            <a href="#" data-target="mobile-nav" class="sidenav-trigger"
+              ><i class="material-icons">menu</i></a
+            >
+            <ul class="right hide-on-med-and-down">
+              <li><a href="#home">Home</a></li>
+              <li><a href="#search">Search</a></li>
+              <li><a href="#products">Products</a></li>
+              <li><a href="#about">About Us</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <!-- sidenav -->
+    <ul id="mobile-nav" class="sidenav">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#search">Search</a></li>
+      <li><a href="#products">Products</a></li>
+      <li><a href="#about">About Us</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+ 
+  <!-- showcase slider -->
+  <section class="slider">
+    <ul class="slides">
+      <li>
+        <img src="img/polyester.jpg" />
+        <!-- random image -->
+        <div class="caption center-align">
+          <h1>Polyester Printing Mesh</h1>
+          <p class="light grey-text text-lighten-3">
+            Screen printing can transfer thick ink deposits and creat special
+            effects to make it to be optimal process to print high quality
+            products for textile/CDs/glass/ceramics...
+          </p>
+        </div>
+      </li>
+      <li>
+        <img src="img/gloves.jpg" />
+        <!-- random image -->
+        <div class="caption left-align">
+          <h1>Stainless Steel Filter Cloth</h1>
+          <p class="light grey-text text-lighten-3">
+            Widely used for filtering of liquid, solid particles, powders, dust
+            in grain, medicine, ceramic, glass, food, scientific research, mine,
+            coal and other fields.
+          </p>
+        </div>
+      </li>
+      <li>
+        <img src="img/polyester1.jpg" />
+        <!-- random image -->
+        <div class="caption right-align">
+          <h1>Polyester Spiral Press-Filter Fabrics</h1>
+          <p class="light grey-text text-lighten-3">
+            Polyester Spiral Press-filter Fabrics are mainly used on belt type
+            filter press,belt vacuum washing machine are add the filler yarn
+            into the spiral loop, so the air permeability is reduced and less
+            material loss on the surface of the fabric.
+          </p>
+        </div>
+      </li>
+      <li>
+        <img src="img/coffefilter.jpg" />
+        <!-- random image -->
+        <div class="caption center-align">
+          <h1>
+            Stainless Steel Reusable Drip Cone Coffee Filter
+          </h1>
+          <p class="light grey-text text-lighten-3">
+            In one clever and simple-to-use stand alone drip coffee cone that
+            gives you the power of a professional barista at home or when you
+            travel
+          </p>
+        </div>
+      </li>
+    </ul>
+  </section>
+  <!-- Footer -->
+  <!-- <footer class="section brown lighten-1 white-text">
+    <div class="footer-copyright">
+            <div class="container">
+            © 2019 Hengshui Qualified Filter Screen Co., Ltd. 
+            <div class="right"">All rights reserved.</div>
+            </div>
+      </div> -->
+  <!-- <p>xxxxxxxxx</p> -->
+  <!-- </footer> --></body>
